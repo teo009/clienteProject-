ed7e22563b299e96c2ae8e51b071bd860b2005cd
teo009 <teo.obando09@gmail.com>
2021-09-07 12:10:55 -0600

    Agregando una nueva vista de Servicios, Servicio_Activity

--- app/src/main/java/com/example/clientes/HomeActivity.kt
+++ app/src/main/java/com/example/clientes/HomeActivity.kt
@@ -1,11 +1,24 @@
 package com.example.clientes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        fbtClient.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        fbtNuevoCliente.setOnClickListener{
+            val intent = Intent(this, NuevoClienteActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/clientes/ServicioActivity.kt
@@ -0,0 +1,11 @@
+package com.example.clientes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ServicioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_servicio)
+    }
+}
